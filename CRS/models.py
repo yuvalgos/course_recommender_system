c17400e14e55264fe8d5b88fc445909b175b7158
yuval <yuvalgos@gmail.com>
2020-08-14 23:13:48 +0300

    in

---
+++ CRS/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Search(models.Model):
+    search = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.search)
+
+    class Meta:
+        verbose_name_plural = "Searches"
+

26d5f863bc2d90e4372da3d170b42b4bfa0771fa
yuval <yuvalgos@gmail.com>
2020-08-22 14:58:36 +0300

    add frontend and req files

--- CRS/views.py
+++ CRS/views.py
@@ -1,6 +1,15 @@
+import requests
 from django.shortcuts import render
-
+from bs4 import BeautifulSoup
 
 # Create your views here.
 def home(request):
     return render(request, template_name='base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    print(search)
+    data_to_show = {
+        'search': search,
+    }
+    return render(request, 'CRS/new_search.html', data_to_show)

3e81f829d775ef770006c43198ecc49989e4764a
yuval <yuvalgos@gmail.com>
2020-10-29 14:46:24 +0200

    added basic recommender train logic

--- CRS/recommender.py
+++ CRS/recommender.py
@@ -38,7 +38,8 @@ def train_recommender():
     dump.dump("RecommenderDumpWL", algo=algorithm_workload)
 
     f = open("recommender_log.txt", "a")
-    f.write(">>>> recommender trained in ", time.time() - start_time, "seocnds")
+    log = ">>>> recommender trained in " + time.time() - start_time + "seocnds"
+    f.write(log)
     f.close()
 
 
